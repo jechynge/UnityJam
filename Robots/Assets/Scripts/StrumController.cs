60762b3ffc388643728c8adf1dd2908e0d6b52d5
jechynge <ching.jordan@gmail.com>
2015-07-19 23:53:02 -0600

    Objects with Grabable tag can be pushed

--- Robots/Assets/Scripts/StrumController.cs
+++ Robots/Assets/Scripts/StrumController.cs
@@ -1,22 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FretController : MonoBehaviour {
-
+public class StrumController : MonoBehaviour {
+	
 	public float maxSpeed = 7.5f;
-	public float jumpForce = 400f;
 	public Transform groundCheck;
 	public LayerMask whatIsGround;
 	
 	Animator anim;
 	Rigidbody2D body;
-
+	
 	bool facingRight = true;
-	bool canJump = true;
 	bool canTrigger = true;
 	bool grounded = false;
 	float groundRadius = 0.2f;
-
+	
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
@@ -25,37 +23,34 @@ public class FretController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("grounded", grounded);
 		anim.SetFloat ("vSpeed", body.velocity.y);
-
-
-		float move = Input.GetAxis ("FretHorizontal");
-
+		
+		
+		float move = Input.GetAxis ("StrumHorizontal");
+		
 		anim.SetFloat ("speed", Mathf.Abs (move));
-
+		
 		body.velocity = new Vector2 (move * maxSpeed, body.velocity.y);
-
+		
 		if (move > 0 && !facingRight) {
 			Flip ();
 		} else if (move < 0 && facingRight) {
 			Flip ();
 		}
 	}
-
+	
 	void Update () {
-		if(Input.GetAxis ("FretJump") == 0)
-			canJump = true;
-
 		if (Input.GetAxis ("FretInteract") == 0)
 			canTrigger = true;
-
-		if (Input.GetAxis ("FretJump") > 0 && grounded && canJump) {
-			canJump = false;
-			grounded = false;
-			anim.SetBool("grounded", grounded);
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+	}
+	
+	void OnTriggerStay2D(Collider2D coll) {
+		if (Input.GetAxis ("FretInteract") > 0 && coll.gameObject.tag == "Interactive" && canTrigger) {
+			canTrigger = false;
+			coll.gameObject.GetComponent<TriggerSender> ().SendTrigger ();
 		}
 	}
 
@@ -71,14 +66,7 @@ public class FretController : MonoBehaviour {
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 		body.velocity = pushDir * pushPower;
 	}
-
-	void OnTriggerStay2D(Collider2D coll) {
-		if (Input.GetAxis ("FretInteract") > 0 && coll.gameObject.tag == "Interactive" && canTrigger) {
-			canTrigger = false;
-			coll.gameObject.GetComponent<TriggerSender> ().SendTrigger ();
-		}
-	}
-
+	
 	void Flip() {
 		facingRight = !facingRight;
 		Vector3 scale = transform.localScale;
