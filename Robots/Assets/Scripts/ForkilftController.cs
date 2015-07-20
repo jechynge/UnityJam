38acface0f336f2f8b80b919729cc8bcc65b99af
ryanbromsgrove <ryan.bromsgrove@gmail.com>
2015-07-19 20:23:12 -0600

    Added raising forklift platform

---
+++ Robots/Assets/Scripts/ForkilftController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForkilftController : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.SetBool ("movedown", true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onTrigger(bool triggered) {
+		if (triggered) {
+			animator.SetBool ("movedown", false);
+			animator.SetBool ("moveup", true);
+		} else {
+			animator.SetBool ("moveup", false);
+			animator.SetBool ("movedown", true);
+		}
+	}
+}
