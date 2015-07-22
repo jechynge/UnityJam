bd50de156cd95e5b89761094810ad7b2e9e968a9
ryanbromsgrove <ryan.bromsgrove@gmail.com>
2015-07-21 21:16:10 -0600

    Weight dropping and crushing
    Weights tagged Crusher will now destroy things with CrushingController
    on them, and then fall off the screen.

---
+++ Robots/Assets/CrushingController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrushingController : MonoBehaviour {
+	Rigidbody2D rigidbody;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (collision.gameObject.tag == "Crusher") {
+			gameObject.GetComponent<Animator> ().SetBool ("crushed", true);
+			rigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+			//rigidbody.isKinematic = true;
+			collision.collider.isTrigger = true;
+			collision.gameObject.GetComponent <Animator>().SetBool("timetofall", true);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
