ba248631b4a104a8cd5fac09a84ffd873b3e4cb6
jechynge <ching.jordan@gmail.com>
2015-07-22 21:51:27 -0600

    added strum's attack animation

--- Robots/Assets/Scripts/StrumController.cs
+++ Robots/Assets/Scripts/StrumController.cs
@@ -9,21 +9,17 @@ public class StrumController : MonoBehaviour {
 	
 	Animator anim;
 	Rigidbody2D body;
-    DistanceJoint2D dragJoint;
 	
 	bool facingRight = true;
 	bool canSwing = true;
     bool canSmash = true;
 	bool grounded = false;
 	float groundRadius = 0.2f;
-    float maxDragDistance = 2.0f;
 	
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		body = GetComponent<Rigidbody2D> ();
-        dragJoint = GetComponent<DistanceJoint2D>();
-        dragJoint.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -32,7 +28,6 @@ public class StrumController : MonoBehaviour {
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("grounded", grounded);
 		anim.SetFloat ("vSpeed", body.velocity.y);
-		
 		
 		float move = Input.GetAxis ("StrumHorizontal");
 		
@@ -53,39 +48,7 @@ public class StrumController : MonoBehaviour {
 
         if (Input.GetAxis("StrumSmash") == 0)
             canSmash = true;
-
-        // If we have the grab key down and we're not currently dragging something...
-        if (Input.GetAxis("StrumGrab") > 0 && dragJoint.connectedBody == null)
-        {
-            // ... find the closest object ...
-            GameObject closest = GetClosestDraggable();
-
-            // ... and if it exists, drag it
-            if (closest != null)
-            {
-                DragObject(closest);
-            }
-
-        }
-        // otherwise, if we don't have the grab key pressed and we're dragging something...
-        else if(Input.GetAxis("StrumGrab") == 0 && dragJoint.connectedBody != null)
-        {
-            // ...release the dragged object
-            ReleaseDraggedObject();
-        }
 	}
-
-    void DragObject(GameObject draggable)
-    {
-        dragJoint.connectedBody = draggable.GetComponent<Rigidbody2D>();
-        dragJoint.enabled = true;
-    }
-
-    void ReleaseDraggedObject()
-    {
-        dragJoint.connectedBody = null;
-        dragJoint.enabled = false;
-    }
 	
 	void Flip() {
 		facingRight = !facingRight;
@@ -93,30 +56,4 @@ public class StrumController : MonoBehaviour {
 		scale.x *= -1;
 		transform.localScale = scale;
 	}
- 
-     GameObject GetClosestDraggable () 
-     {
-         GameObject closest = null;
-         float closestDistance = Mathf.Infinity;
-         GameObject[] draggables = GameObject.FindGameObjectsWithTag("Draggable");
-
-         foreach(GameObject o in draggables) {
-             float distance = Mathf.Abs(Vector3.Distance(transform.position, o.transform.position));
-
-             // Item is "behind" the player and shouldn't be dragged
-             if(facingRight && transform.position.x - o.transform.position.x > 0) 
-                 continue;
-
-             if (!facingRight && transform.position.x - o.transform.position.x < 0)
-                 continue;
-
-             if (distance < closestDistance)
-             {
-                 closest = o;
-                 closestDistance = distance;
-             }
-         }
-     
-         return closestDistance <= maxDragDistance ? closest : null;
-     }
 }
