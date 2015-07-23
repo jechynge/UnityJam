2ee56b2cf62198dff30d8dd2cb6f9b00cb0ccbc1
ryanbromsgrove <ryan.bromsgrove@gmail.com>
2015-07-22 19:49:32 -0600

    Scene Exit
    Can now transition between scenes! (make sure scene you want to go to
    is in the build, otherwise should be pretty self-explanatory)

---
+++ Robots/Assets/Scripts/SceneExit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneExit : MonoBehaviour {
+
+	public string nextScene;
+	private bool fretIn;
+	private bool strumIn;
+
+	// Use this for initialization
+	void Start () {
+		fretIn = false;
+		strumIn = false;
+	}
+
+
+	void OnTriggerStay2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player")
+			fretIn = true;
+		if (collider.gameObject.tag == "Player 2") 
+			strumIn = true;
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (fretIn && strumIn) {
+			Debug.Log ("Changing level");
+			Application.LoadLevel (nextScene);
+		}
+		fretIn = false;
+		strumIn = false;
+
+	}
+}
